2445484fe95495c6b043d5611daf3f6b7ce05e5c
AldiPradanahakim <aldi.pradan.hakim26@gmail.com>
2025-01-05 03:00:28 +0700

    mengerjakan pertemuan 14

--- src/Pertemuan14/JavaApplication11/src/javaapplication11/JavaApplication11.java
+++ src/Pertemuan14/JavaApplication11/src/javaapplication11/JavaApplication11.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package pertemuan13;
+package javaapplication11;
 
 /**
  *
  * @author aldip
  */
-public class Pertemuan13 {
+public class JavaApplication11 {
 
     /**
      * @param args the command line arguments
